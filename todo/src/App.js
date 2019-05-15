060fcc431e51a3c1f42cbfc0d6961ad221c29d9e
Vijay Das <vijaytdas@gmail.com>
2019-05-14 21:39:44 -0500

    refactor app.js to create TodoList component

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,60 +1,16 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { addTodo, toggleComplete } from './actions';
+import React from "react";
+import "./App.css";
+// import { connect } from "react-redux";
+// import { addTodo, toggleComplete } from "./actions";
+// import { Provider } from "react-redux";
+import TodoList from "./components/TodoList";
 
-class App extends React.Component {
-  state = {
-    newTodo: ''
-  };
-
-  handleChanges = e => {
-    this.setState({ newTodo: e.target.value });
-  };
-
-  addTodo = e => {
-    e.preventDefault();
-    this.props.addTodo(this.state.newTodo);
-    this.setState({ newTodo: '' });
-  };
-
-  toggleComplete = id => {
-    this.props.toggleComplete(id);
-  };
-
-  render() {
-    return (
-      <React.Fragment>
-        <div className="todo-list">
-
-        {this.props.todos &&
-            this.props.todos.map((todo) => (
-              <h4 onClick={() => this.toggleComplete(todo.id)} key={todo.id}>
-                {todo.value}
-                {todo.completed && <i class="fas fa-check-circle"></i>}
-              </h4>
-            ))}
-
-        </div>
-
-        <input
-          type="text"
-          value={this.state.newTodo}
-          onChange={this.handleChanges}
-          placeholder="Add new item"
-        />
-        <button onClick={this.addTodo}>Add Item</button>
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
 }
 
-const mapStateToProps = state => {
-  return {
-    todos: state.todos
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  { addTodo, toggleComplete }
-)(App);
+export default App;
